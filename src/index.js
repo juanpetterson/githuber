d573d55022cae3ad2388637821a7ecaf420eb755
juanpetterson <juan.petterson@gmail.com>
2019-06-02 10:39:08 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
+
+import "~/config/ReactotronConfig";
+import "~/config/DevToolsConfig";
+
+import createNavigator from "./routes";
+
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem("@Githuber:username");
+
+    this.setState({ userChecked: true, userLogged: !!username });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
---
+++ src/pages/Organizations/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList
+} from "react-native";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import Header from "~/components/Header";
+import OrganizationItem from "./OrganizationItem";
+import styles from "./styles";
+
+import api from "~/services/api";
+
+const TabIcon = ({ tintColor }) => (
+  <Icon name="building" size={20} color={tintColor} />
+);
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired
+};
+
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon
+  };
+
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false
+  };
+
+  async componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+    const username = await AsyncStorage.getItem("@Githuber:username");
+    const { data } = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadOrganizations}
+        refreshing={refreshing}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? (
+          <ActivityIndicator style={styles.loading} />
+        ) : (
+          this.renderList()
+        )}
+      </View>
+    );
+  }
+}
